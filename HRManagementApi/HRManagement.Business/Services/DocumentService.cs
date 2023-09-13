aa524e2ffd406e8ca79f796dc9a4d71e96a0355c
Manuel Marginean <manuel.marginean@red-to-blue.com>
2023-09-13 15:45:54 +0300

    Deleted unused func

--- HRManagementApi/HRManagement.Business/Services/DocumentService.cs
+++ HRManagementApi/HRManagement.Business/Services/DocumentService.cs
@@ -14,23 +14,7 @@ namespace HRManagement.Business.Services
             _documentRepository = documentRepository ?? throw new ArgumentNullException(nameof(documentRepository));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
-        public async Task<IEnumerable<DocumentDto>> GetDocuments(long customerId)
-        {
-            if (!await _customerService.CustomerExists(customerId))
-            {
-                throw new NotFoundException();
-            }
-            var documentsForCustomer = await _documentRepository.GetDocumentsForCustomerAsync(customerId);
-            return _mapper.Map<IEnumerable<DocumentDto>>(documentsForCustomer);
-        }
-        public async Task<DocumentDto> GetDocument(long customerId, long documentId)
-        {
-            var document = await _documentRepository.GetDocumentForCustomerAsync(customerId, documentId);
-            if ((!await _customerService.CustomerExists(customerId)) || (document == null))
-            {
-                throw new NotFoundException();
-            }
-            return _mapper.Map<DocumentDto>(document);
-        }
+       
+       
     }
 }
