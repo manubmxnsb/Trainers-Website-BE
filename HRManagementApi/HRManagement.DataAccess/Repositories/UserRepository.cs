cee9627d136e55703851d3d492c10fa40d4b9c92
Marioara Pavelea <marioara.pavelea@red-to-blue.com>
2023-09-15 13:34:31 +0300

    small changes

--- HRManagementApi/HRManagement.DataAccess/Repositories/UserRepository.cs
+++ HRManagementApi/HRManagement.DataAccess/Repositories/UserRepository.cs
@@ -16,7 +16,7 @@ namespace HRManagement.DataAccess.Repositories
 
         public async Task<User?> GetUserAsync(long userId)
         { 
-                return await _context.Users.Include(u => u.Events).Where(u => u.Id == userId).FirstOrDefaultAsync();       
+            return await _context.Users.Include(u => u.Events).Where(u => u.Id == userId).FirstOrDefaultAsync();       
         }
 
         public async Task<bool> UserExistsAsync(long userId)
