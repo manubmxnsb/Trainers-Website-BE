00a33f7bf0bb96bca3093a61d167602c33993599
Nelus Andrada <andrada.nelus@red-to-blue.com>
2023-09-27 14:09:18 +0300

    dtos readded

---
+++ HRManagementApi/HRManagement.Business/Models/CustomerDTO.cs
@@ -0,0 +1,31 @@
+﻿using HRManagement.DataAccess.Enums;
+
+namespace HRManagement.Business.Models
+{
+    public class CustomerDto
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Country { get; set; }
+
+        public decimal? VAT { get; set; }
+
+        public BillingType? BillingType { get; set; }
+
+        public string? Details { get; set; }
+
+        public bool IsActive { get; set; } = false;
+
+        public DateTime? DateCreated { get; set; }
+
+        public ICollection<DocumentDto> Documents { get; set; } = new List<DocumentDto>();
+    }
+}
---
+++ HRManagementApi/HRManagement.Business/Models/DocumentDTO.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace HRManagement.Business.Models
+{
+    public class DocumentDto
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime? CreationDate { get; set; }
+
+        public byte[]? Content { get; set; }
+    }
+}
